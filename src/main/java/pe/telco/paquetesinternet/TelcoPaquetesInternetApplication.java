d38751c780e7c9fe6d421851adb4d1ef15f5fa82
rodcar <ivanrod12@hotmail.com>
2023-01-04 23:41:22 -0500

    commit inicial

---
+++ src/main/java/pe/telco/paquetesinternet/TelcoPaquetesInternetApplication.java
@@ -0,0 +1,13 @@
+package pe.telco.paquetesinternet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoPaquetesInternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoPaquetesInternetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/telco/paquetesinternet/TelcoPaquetesInternetApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.telco.paquetesinternet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoPaquetesInternetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
