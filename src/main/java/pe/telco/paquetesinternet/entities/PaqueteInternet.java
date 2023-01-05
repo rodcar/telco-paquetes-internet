19f396ec0d3a40dca7698d78d41143645290dbea
rodcar <ivanrod12@hotmail.com>
2023-01-04 23:41:42 -0500

    agrega la entidad PaqueteInternet

---
+++ src/main/java/pe/telco/paquetesinternet/entities/PaqueteInternet.java
@@ -0,0 +1,25 @@
+package pe.telco.paquetesinternet.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "paquetes_internet")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaqueteInternet {
+
+	@Id
+	private String id;
+	private String nombre;
+	private String descripcion;
+	private Double precio;
+}
